88feca5c888bf6401c55a92bbbfd17876c89d72a
Daniyil Vasylenko <danielspr3@gmail.com>
2019-06-05 03:05:55 +0300

    Add license file. Fixed icon artifacts.

--- main/utils.js
+++ main/utils.js
@@ -2,7 +2,13 @@ const path = require('path');
 
 module.exports = {
   fixIcon: (windowConfig) => {
-    if (process.platform !== 'win32' && process.platform !== 'win64') {
+    if (process.platform === 'darwin') {
+      return {
+        ...windowConfig,
+        icon: path.join(__dirname, '../assets/icon.icns')
+      };
+    }
+    if (process.platform === 'linux') {
       return {
         ...windowConfig,
         icon: path.join(__dirname, '../assets/icon.png')
