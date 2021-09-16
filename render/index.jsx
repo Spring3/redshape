c471276d0d51af7119a8bfb92cb06d34a4ff489e
Daniel Vasylenko <danielspr3@gmail.com>
2021-09-16 09:11:37 +0200

    feat: setup overmind

--- render/index.jsx
+++ render/index.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { Provider as OvermindProvider } from 'overmind-react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
+import { createOvermind } from 'overmind';
 import store from './store';
 import theme from './theme';
 import App from './App';
+import { createStoreConfig } from './store/index';
 
 if (module.hot) {
   module.hot.accept();
 }
 
+const overmindStore = createOvermind(createStoreConfig());
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <HashRouter>
-        <App dispatch={store.dispatch} />
+        <OvermindProvider value={overmindStore}>
+          <App dispatch={store.dispatch} />
+        </OvermindProvider>
       </HashRouter>
     </Provider>
   </ThemeProvider>,
