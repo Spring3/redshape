cda6e59d2425ddb5c370f4905469f71716f439cc
Daniel Vasylenko <connect.danv@gmail.com>
2022-03-15 04:29:29 +0100

    chore: cleanup timeEntries reducer

--- render/actions/index.js
+++ render/actions/index.js
@@ -1,9 +1,7 @@
 import issueActions from './issue.actions';
-import timeEntryActions from './timeEntry.actions';
 import settingsActions from './settings.actions';
 
 export default {
   issue: issueActions,
-  timeEntry: timeEntryActions,
   settings: settingsActions
 };
